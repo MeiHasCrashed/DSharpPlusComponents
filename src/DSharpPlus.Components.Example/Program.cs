eb5ef321426077eafb5cc3aad1fe92cccd8ea9d6
MeiHasCrashed <meihascrashed@outlook.com>
2025-11-04 16:10:33 +0100

    Updated example bot to have a button for testing.

--- src/DSharpPlus.Components.Example/Program.cs
+++ src/DSharpPlus.Components.Example/Program.cs
@@ -1,15 +1,36 @@
-﻿using DSharpPlus;
+﻿using System.Reflection;
+using DSharpPlus;
+using DSharpPlus.Commands;
+using DSharpPlus.Commands.Processors.TextCommands;
+using DSharpPlus.Commands.Processors.TextCommands.Parsing;
 using DSharpPlus.Components.Example;
+using DSharpPlus.Components.Extensions;
 using DSharpPlus.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var builder = Host.CreateApplicationBuilder(args);
 
 var token = Environment.GetEnvironmentVariable("DISCORD_TOKEN") ?? throw new InvalidOperationException("DISCORD_TOKEN environment variable not set");
 
+builder.Logging
+    .ClearProviders()
+    .SetMinimumLevel(LogLevel.Debug)
+    .AddSimpleConsole(c => c.SingleLine = true);
+
 builder.Services
-    .AddDiscordClient(token, DiscordIntents.AllUnprivileged)
+    .AddDiscordClient(token, DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents)
+    .AddComponentsExtension()
+    .AddCommandsExtension((_, commands) =>
+    {
+        commands.AddCommands(Assembly.GetAssembly(typeof(BotService))!);
+        var textProcessor = new TextCommandProcessor(new TextCommandConfiguration
+        {
+            PrefixResolver = new DefaultPrefixResolver(true, "!").ResolvePrefixAsync
+        });
+        commands.AddProcessor(textProcessor);
+    }, new CommandsConfiguration{ RegisterDefaultCommandProcessors = false})
     .AddHostedService<BotService>();
     
 var host = builder.Build();
---
+++ src/DSharpPlus.Components.Example/TestCommands.cs
@@ -0,0 +1,17 @@
+﻿using DSharpPlus.Commands;
+using DSharpPlus.Commands.Processors.TextCommands;
+using DSharpPlus.Entities;
+
+namespace DSharpPlus.Components.Example;
+
+public class TestCommands
+{
+    [Command("button")]
+    public async Task ButtonCommand(TextCommandContext ctx)
+    {
+        var builder = new DiscordMessageBuilder()
+            .WithContent("Example button message:")
+            .AddActionRowComponent(new DiscordButtonComponent(DiscordButtonStyle.Primary, "example_button-245a-48mf", "Click Me!"));
+        await ctx.Channel.SendMessageAsync(builder);
+    }
+}
