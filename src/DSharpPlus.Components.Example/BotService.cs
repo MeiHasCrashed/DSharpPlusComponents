39ec79a034268b31b13ee8f5f13717ffb9d5c3b7
MeiHasCrashed <meihascrashed@outlook.com>
2025-11-04 15:37:15 +0100

    Add very simple bot for testing purposes.

---
+++ src/DSharpPlus.Components.Example/BotService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace DSharpPlus.Components.Example;
+
+public class BotService(ILogger<BotService> logger, DiscordClient client) : IHostedService
+{
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await client.ConnectAsync();
+        logger.LogInformation("Bot is connected and running.");
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await client.DisconnectAsync();
+    }
+}
--- src/DSharpPlus.Components.Example/Program.cs
+++ src/DSharpPlus.Components.Example/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using DSharpPlus;
+using DSharpPlus.Components.Example;
+using DSharpPlus.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+var token = Environment.GetEnvironmentVariable("DISCORD_TOKEN") ?? throw new InvalidOperationException("DISCORD_TOKEN environment variable not set");
+
+builder.Services
+    .AddDiscordClient(token, DiscordIntents.AllUnprivileged)
+    .AddHostedService<BotService>();
+    
+var host = builder.Build();
+host.Run();
